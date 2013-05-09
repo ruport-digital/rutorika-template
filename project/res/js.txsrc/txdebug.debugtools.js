834814a9044a648245eaa10ed6cfff81353e16ec
uncleF <uncle.funkay@gmail.com>
2013-05-10 03:39:53 +0400

    Project Changes

--- project/res/js.txsrc/txdebug.debugtools.js
+++ project/res/js.txsrc/txdebug.debugtools.js
@@ -1,39 +1,26 @@
-var DEBUG_TOOLS = '<div class="debugTools"><ul class="lstRst"><li><label class="dTls-VR"><input type="checkbox" name="dTls-Section1" class="dTls-VRCheck"> Vertical Rhythm</label></li><li><label class="dTls-RG"><input type="checkbox" name="dTls-Section1" class="dTls-RGCheck"> Responsive Guide</label></li></ul><a href="#" class="debugToolsToggle"></a></div>';
+var DEBUG_TOOLS = '<div class="debugTools off"><ul class="lstRst"><li><label class="dTls-VR"><input type="checkbox" name="dTls-Section1" class="dTls-VRCheck"> Vertical Rhythm</label></li><li><label class="dTls-RG"><input type="checkbox" name="dTls-Section1" class="dTls-RGCheck"> Responsive Guide</label></li></ul><a href="#" class="debugToolsToggle"></a></div>';
 
 function initDebug() {
-	$(DEBUG_TOOLS).appendTo("body")
+	$(DEBUG_TOOLS).appendTo("body");
 }
 
 $(document).ready(function() {
 
 	initDebug();
 
+	var BODY = $("body");
+
 	$(".dTls-VRCheck").on("change", function(event) {
-		var BODY = $("body");
-		if (BODY.hasClass("debugRhythmLines")) {
-			BODY.removeClass("debugRhythmLines")
-		} else {
-			BODY.addClass("debugRhythmLines")
-		}
+		BODY.toggleClass("debugRhythmLines");
 	});
 
 	$(".dTls-RGCheck").on("change", function(event) {
-		var BODY = $("body");
-		if (BODY.hasClass("debugResponsiveGuide")) {
-			BODY.removeClass("debugResponsiveGuide")
-		} else {
-			BODY.addClass("debugResponsiveGuide")
-		}
+		BODY.toggleClass("debugResponsiveGuide");
 	});
 
 	$(".debugToolsToggle").on("click", function(event) {
 		event.preventDefault();
-		var OBJECT = $(".debugTools");
-		if (OBJECT.hasClass("off")) {
-			OBJECT.removeClass("off")
-		} else {
-			OBJECT.addClass("off")
-		}
-	})
+		$(".debugTools").toggleClass("off");
+	});
 
 });
