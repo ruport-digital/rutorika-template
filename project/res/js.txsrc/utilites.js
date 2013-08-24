6c70516730e9d782be04c4b84e7ca65922832440
uncle Fucka <uncleFucka@uncle-Fucka-X.local>
2013-08-24 23:36:16 +0400

    Project Changes

---
+++ project/res/js.txsrc/utilites.js
@@ -0,0 +1,25 @@
+function whichDevice() {
+	var USER_AGENT_STRING = navigator.userAgent.toLowerCase();
+	var MOBILE_LIST = new Array("iphone os 5", "ipad; cpu os 5", "iphone", "ipad", "android 2", "android", "blackberry", "palmos");
+	for (var DEVICE in MOBILE_LIST) {
+		if (USER_AGENT_STRING.indexOf(MOBILE_LIST[DEVICE])>=0) {
+			return MOBILE_LIST[DEVICE];
+		}
+	}
+}
+
+function whichTransitionEvent() {
+	var TRANSITION;
+	var ELEMENT = document.createElement('fakeelement');
+	var TRANSITIONS = {
+		"transition": "transitionend",
+		"OTransition": "oTransitionEnd",
+		"MozTransition": "transitionend",
+		"WebkitTransition": "webkitTransitionEnd"
+	};
+	for(TRANSITION in TRANSITIONS){
+		if(ELEMENT.style[TRANSITION] !== undefined) {
+			return TRANSITIONS[TRANSITION];
+		}
+	}
+}
