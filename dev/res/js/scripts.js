8487f74f47f2047c267bf3bb10fe65c90e2a88d8
uncleF <uncle.funkay@gmail.com>
2016-01-14 07:36:57 +0300

    Project Update

--- dev/res/js/scripts.js
+++ dev/res/js/scripts.js
@@ -30,12 +30,6 @@
     function (require, module, exports) {
       'use strict';
       (function () {
-        function sum(a, b) {
-          return a + b;
-        }
-        var s = sum(100, 200);
-        s = 0;
-        console.log('!');
       }());
     },
     {}
