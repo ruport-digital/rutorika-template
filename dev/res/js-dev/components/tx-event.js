b152b800ce844991e2e74db65e3867f856dbe1ca
uncleF <uncle.funkay@gmail.com>
2016-03-02 19:11:01 +0300

    Project Update

--- dev/res/js-dev/components/tx-event.js
+++ dev/res/js-dev/components/tx-event.js
@@ -19,8 +19,8 @@ function unbind(object, type, callback) {
 }
 
 function trigger(object, event, propagate) {
-  propagate = propagate || false;
   var eventObj;
+  propagate = propagate || false;
   if (document.createEvent) {
     eventObj = document.createEvent('MouseEvents');
     eventObj.initEvent(event, propagate, false);
