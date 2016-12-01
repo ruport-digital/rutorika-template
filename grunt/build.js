71f41d233331307395c6266ad199da7b51801378
Ilya Biryukov <uncle.funkay@gmail.com>
2016-12-01 15:34:18 +0300

    Project Update

--- grunt/build.js
+++ grunt/build.js
@@ -15,15 +15,27 @@ module.exports = (grunt) => {
   grunt.registerTask('build-finalize', [
     'string-replace:build',
     'cleanempty',
-    'test'
   ]);
 
   grunt.registerTask('build', [
+    'build-resources',
+    'build-finalize',
+    'test'
+  ]);
+
+  grunt.registerTask('build-fast', [
     'build-resources',
     'build-finalize'
   ]);
 
   grunt.registerTask('build-critical', [
+    'build-resources',
+    'compile-critical',
+    'build-finalize',
+    'test'
+  ]);
+
+  grunt.registerTask('build-critical-fast', [
     'build-resources',
     'compile-critical',
     'build-finalize'
