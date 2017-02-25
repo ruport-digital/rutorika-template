60a060110faf96c6be92110eb38c9b4e54b40ba1
uncleF <uncle.funkay@gmail.com>
2017-02-25 21:58:39 +0300

    Project Update

--- grunt/tasks/string-replace.js
+++ grunt/tasks/string-replace.js
@@ -34,9 +34,6 @@ module.exports = (grunt, options) => {
         }, {
           pattern: /(?:\r?\n|\r)<\/noscript>/gi,
           replacement: '</noscript>',
-        }, {
-          pattern: /(\s[а-яА-Яa-zA-Z]{1,2})\s/gi,
-          replacement: '$1&nbsp;',
         }],
       },
       files: {
